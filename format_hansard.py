7dd7ce139426cec72daa0fdfa1a16325373fb005
nigelnnk <17807884+nigelnnk@users.noreply.github.com>
2021-04-10 17:22:26 +0800

    Include cleaning of author names in formatter

--- format_hansard.py
+++ format_hansard.py
@@ -8,8 +8,8 @@ parser = argparse.ArgumentParser(description="Removes HTML tags from Hansard spe
 parser.add_argument("path", type=str, help="path to Hansard JSON file")
 parser.add_argument("-s", "--session", action="store_true",
                     help="Invoke parser for full hansard sessions, else parse other formats (e.g. bills, motions)")
-parser.add_argument("-f", "--full", action="store_true",
-                    help="remove ALL peripheral text (see below)")
+parser.add_argument("-g", "--granular", action="store_true",
+                    help="enable granular options for removal of text (see below)")
 parser.add_argument("-v", "--vernacular", action="store_true",
                     help="remove vernacular references from text (e.g. 'In Mandarin')")
 parser.add_argument("-a", "--applause", action="store_true",
@@ -18,6 +18,8 @@ parser.add_argument("-t", "--timestamp", action="store_true",
                     help="remove timestamps from text (e.g. '1.18pm')")
 parser.add_argument("-p", "--procedural", action="store_true",
                     help="remove procedural text demarcated by (proc test)")
+parser.add_argument("-pg", "--pages", action="store_true",
+                    help="remove page numbers")
 
 if __name__ == "__main__":
     args = parser.parse_args()
@@ -36,40 +38,69 @@ if __name__ == "__main__":
         speeches = [raw_data["resultHTML"]]
 
     sessions = []
+    speaker_list = {}
     for raw_speech in speeches:
         out_json = {}
         html_content = raw_speech["content"]
         soup = BeautifulSoup(html_content, "html.parser")
         text = soup.get_text(separator='\n')
 
-        if args.vernacular or args.full:
+        if args.vernacular or not args.granular:
             text = re.sub(r"\(\s+In Mandarin\s+\)\s+:\s+\[\s+Please refer to\s+Vernacular Speech\n+\.]", "", text)
             text = re.sub(r"\(\s+In Malay\s+\)\s+:\s+\[\s+Please refer to\s+Vernacular Speech\n+\.]", "", text)
             text = re.sub(r"\(\s*In English\s*\):?", "", text)
             text = re.sub(r"\(\s*In Malay\s*\):?", "", text)
             text = re.sub(r"\(\s*In Mandarin\s*\):?", "", text)
 
-        if args.applause or args.full:
+        if args.applause or not args.granular:
             text = re.sub(r"\[\s+Applause.?\s+]", "", text)
 
-        if args.timestamp or args.full:  # only matches timestamps that are in its own line
+        if args.timestamp or not args.granular:  # only matches timestamps that are in its own line
             text = re.sub(r"[\r\n]+\d{1,2}\.\d{2}[ ]?[ap]m[\r\n]", "\n", text)
 
-        if args.procedural or args.full:
+        if args.procedural or not args.granular:
             text = re.sub(r"\[\(proc text\).+?\(proc text\)]", "", text)
 
+        if args.pages or not args.granular:
+            text = re.sub(r"\sPage: \d{1,4}\s", "", text)
+
         pre_text = text.split('\n')
-        speaker_list = list(soup.find_all("strong"))
-        speaker_list = list(set(map(lambda x: re.sub(r"<.+?>", "", str(x)).strip(), speaker_list)))
+
+        # Generate list of speakers
+        curr_speaker_list = list(soup.find_all("strong"))
+        curr_speaker_list = list(set(map(lambda x: re.sub(r"<.+?>", "", str(x)).strip(), curr_speaker_list)))
+        curr_speaker_list = list(filter(lambda x: x.strip() != "The Chairman", curr_speaker_list))
+
+        # Generate dictionary of speakers to names for formatting later
+        clean_speakers = {}
+        for i in range(len(curr_speaker_list)):
+            speaker = curr_speaker_list[i]
+            if speaker.find("(") != -1:
+                parts = speaker.split("(")
+                if parts[0].find("Minister") != -1 or parts[0].find("Leader") != -1:
+                    if parts[1].lower().find("schools") != -1 or parts[1].lower().find("education") != -1:
+                        name = parts[2].split(")")[0]
+                    else:
+                        name = parts[1].split(")")[0]
+                else:
+                    name = parts[0].strip()
+            else:
+                name = speaker.strip()
+            if len(name) > 0:
+                clean_speakers[speaker] = name
+
+        # Update list of speakers
+        speaker_list.update(clean_speakers)
         speech_json = []
 
+        # Extract speeches, labelling with speaker name when encountered
         current_speaker = ""
         current_speech = []
         for line in pre_text:
             sline = line.strip().replace('\u009d', "")
             if sline in speaker_list:
                 if current_speaker and current_speech:
-                    d = {"speaker": current_speaker, "content": current_speech.copy()}
+                    d = {"speaker": speaker_list[current_speaker], "content": current_speech.copy()}
                     speech_json.append(d.copy())
                 current_speaker = sline
                 current_speech.clear()
@@ -77,15 +108,20 @@ if __name__ == "__main__":
             if sline:
                 current_speech.append(sline)
 
-        if current_speech:
-            d = {"speaker": current_speaker, "content": current_speech.copy()}
+        if current_speaker and current_speech:
+            d = {"speaker": speaker_list[current_speaker], "content": current_speech.copy()}
             speech_json.append(d.copy())
 
+        # Save to Json
         out_json["title"] = raw_speech["title"]
         out_json["speeches"] = speech_json.copy()
-        out_json["speakers"] = speaker_list.copy()
+        out_json["speakers"] = list(set(clean_speakers.values()))
 
         sessions.append(out_json.copy())
 
+    from pprint import pprint
+    pprint(speaker_list)
+
     with open(output_path, "w", encoding="UTF-8") as f:
-        json.dump({"filename": args.path, "sessions": sessions}, f, indent=2, ensure_ascii=False)
+        json.dump({"filename": args.path, "sessions": sessions, "all_speakers": list(set(speaker_list.values()))},
+                  f, indent=2, ensure_ascii=False)
